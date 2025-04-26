f3645dcfd26a9ab2d727b98bf0a9983ea6ce9666
Vitalii Boiko <boyko330@gmail.com>
2025-04-26 14:52:49 +0300

    + rabbit MQ module and config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,6 +6,7 @@ import { webPushConfig, WebPushConfig } from './web-push.config';
 import { GroceryStorageConfig, groceryStorageConfig } from './grocery-storage.config';
 import { gcpConfig, GcpConfig } from './gcp.config';
 import { recipeGeneratorConfig, RecipeGeneratorConfig } from './recipe-generator.config';
+import { rmqConfig, RmqConfig } from './rmq.config';
 
 export interface AppConfig {
   origin: string;
@@ -17,6 +18,7 @@ export interface AppConfig {
   groceryStorageConfig: GroceryStorageConfig;
   gcpConfig: GcpConfig;
   recipeGeneratorConfig: RecipeGeneratorConfig;
+  rmqConfig: RmqConfig;
 }
 
 export const appConfig = (): AppConfig => ({
@@ -29,4 +31,5 @@ export const appConfig = (): AppConfig => ({
   groceryStorageConfig: groceryStorageConfig(),
   gcpConfig: gcpConfig(),
   recipeGeneratorConfig: recipeGeneratorConfig(),
+  rmqConfig: rmqConfig(),
 });
